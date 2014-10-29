bed3c1f27d910cfdbd398e98e722c55065e1031c
Logan Richardson <g.logan.richardson@gmail.com>
2014-10-29 16:39:47 -0500

    merging

--- public/play.js
+++ public/play.js
@@ -183,8 +183,8 @@ var playState = {
   },
 
   render: function(){
-    game.debug.body(player);
-    layer.debug = true;
+    //game.debug.body(player);
+    //layer.debug = true;
   },
 
   playerMovement: function(){
